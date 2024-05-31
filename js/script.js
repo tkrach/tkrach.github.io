c76c62d00694cde3b7b2a9b9844b17401370817b
tkrach <98433403+tkrach@users.noreply.github.com>
2024-05-31 17:53:08 +0900

    Update and rename js/script.js to language-switch.js

--- js/script.js
+++
@@ -1,7 +0,0 @@
-function setLanguage(lang) {
-    if (lang === 'en') {
-        window.location.href = 'index.html';
-    } else if (lang === 'jp') {
-        window.location.href = 'index_jp.html';
-    }
-}
